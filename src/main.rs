8d7a5192686e4aa6ae15575543ddf97c1ab9aa08
Serhii Fedorenko <serhi.fedorenko@gmail.com>
2026-05-16 13:41:45 +0200

    Loading screen moved to utils.rs

--- src/main.rs
+++ src/main.rs
@@ -16,46 +16,7 @@ async fn main() {
     let font =
         load_ttf_font_from_bytes(include_bytes!("../res/font.otf")).expect("Failed to load font");
 
-    let mut loadbar_width = 0;
-
-    for _ in 0..170 {
-        clear_background(BLACK);
-
-        let text = "MNAU!";
-        let font_size = 60;
-
-        // 1. Measure the text using your custom font
-        let dimensions = measure_text(text, Some(&font), font_size, 1.0);
-
-        // 2. Calculate the center of the screen
-        let center_x = screen_width() / 2.0;
-        let center_y = screen_height() / 2.0;
-
-        // 3. Draw using the calculated offsets
-        draw_text_ex(
-            text,
-            center_x - (dimensions.width / 2.0),
-            center_y + (dimensions.height / 2.0) - dimensions.offset_y,
-            TextParams {
-                font: Some(&font),
-                font_size,
-                color: WHITE,
-                ..Default::default()
-            },
-        );
-
-        draw_rectangle(
-            screen_width() / 2.0 - dimensions.width,
-            screen_height() / 2.0 + dimensions.height,
-            loadbar_width as f32,
-            2.0,
-            WHITE,
-        );
-
-        loadbar_width += 2;
-
-        next_frame().await;
-    }
+    utils::load_screen(&font).await;
 
     let assets = match Assets::load(font).await {
         Ok(assets) => assets,
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,49 @@
-use std::fmt::format;
+use macroquad::{
+    color::{BLACK, WHITE},
+    shapes::draw_rectangle,
+    text::{Font, TextParams, draw_text_ex, measure_text},
+    window::{clear_background, next_frame, screen_height, screen_width},
+};
 
-use macroquad::texture::{Texture2D, load_texture};
+pub async fn load_screen(font: &Font) {
+    let mut loadbar_width = 0;
 
-pub async fn load_texture_util(path: &str) -> Texture2D {
-    let error_message = format!("Failed to load texture: {}", path.to_string());
-    load_texture(path).await.expect(&error_message)
+    for _ in 0..170 {
+        clear_background(BLACK);
+
+        let text = "MNAU!";
+        let font_size = 60;
+
+        // 1. Measure the text using your custom font
+        let dimensions = measure_text(text, Some(&font), font_size, 1.0);
+
+        // 2. Calculate the center of the screen
+        let center_x = screen_width() / 2.0;
+        let center_y = { screen_height() / 2.0 };
+
+        // 3. Draw using the calculated offsets
+        draw_text_ex(
+            text,
+            center_x - (dimensions.width / 2.0),
+            center_y + (dimensions.height / 2.0) - dimensions.offset_y,
+            TextParams {
+                font: Some(&font),
+                font_size,
+                color: WHITE,
+                ..Default::default()
+            },
+        );
+
+        draw_rectangle(
+            screen_width() / 2.0 - dimensions.width,
+            screen_height() / 2.0 + dimensions.height,
+            loadbar_width as f32,
+            2.0,
+            WHITE,
+        );
+
+        loadbar_width += 2;
+
+        next_frame().await;
+    }
 }
